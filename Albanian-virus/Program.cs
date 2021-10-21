9aee5327247305ecb10c921554c18c2d40e9584c
vasker <yarinvasket@gmail.com>
2021-10-22 00:05:10 +0300

    run in startup

--- Albanian-virus/Program.cs
+++ Albanian-virus/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IWshRuntimeLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,15 +9,47 @@ namespace Albanian_virus
 {
     static class Program
     {
+        public const string virusMessage = "Hi, I am an Albanian virus but because of poor technology in my country unfortunately I " +
+            "am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me" +
+            " to other users. Many thanks for your cooperation!  Best regards,Albanian virus";
+        public const string titleMessage = "Virus Alert !";
+
+        public const string startUp = "C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\Albanian-virus.lnk";
+        public static string userStartUp = Environment.GetFolderPath(Environment.SpecialFolder.Startup) + "\\Albanian-virus.lnk";
+        public static string appPath = System.Reflection.Assembly.GetEntryAssembly().Location;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            if (!System.IO.File.Exists(startUp))
+            {
+                try
+                {
+                    CreateShortcut(startUp);
+                }
+                catch (Exception)
+                {
+                    CreateShortcut(userStartUp);
+                }
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            MessageBox.Show("Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation!  Best regards,Albanian virus", "Virus Alert !", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+            MessageBox.Show(virusMessage, titleMessage, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+            
+        }
+
+        public static void CreateShortcut(string targetPath)
+        {
+            WshShell shell = new WshShell();
+            string shortcutAddress = targetPath;
+            IWshShortcut shortcut = (IWshShortcut)shell.CreateShortcut(shortcutAddress);
+            shortcut.Description = "";
+            shortcut.Hotkey = "Ctrl+Shift+N";
+            shortcut.TargetPath = appPath;
+            shortcut.Save();
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,11 @@
+# Albanian virus
+Inspired by [this post](https://www.reddit.com/r/masterhacker/comments/hbo7j2)
+
+## About
+This is actually harmless, but keep in mind that it does put itself in your startup
+
+## Download
+Download this from the [releases](https://github.com/yarinvasket/Albanian-virus/releases/latest) tab
+
+## Building
+If you're compiling this yourself, you have to add Project > Add Reference > COM > Windows Script Host Object Model in visual studio.
