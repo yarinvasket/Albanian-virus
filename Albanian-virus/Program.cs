ab690f2bbd89b6e65c4f7431b3eedf92498c787e
vasker <yarinvasket@gmail.com>
2021-10-21 23:38:00 +0300

    Albanian virus

--- Albanian-virus/Program.cs
+++ Albanian-virus/Program.cs
@@ -16,6 +16,7 @@ namespace Albanian_virus
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            MessageBox.Show("Hi, I am an Albanian virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation!  Best regards,Albanian virus", "Virus Alert !", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
         }
     }
 }
